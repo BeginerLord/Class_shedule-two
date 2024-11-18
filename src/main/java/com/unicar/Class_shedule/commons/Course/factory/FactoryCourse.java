74390a95191f26db65badb744d87278da2aff736
rafapsjs <rafaelcorredorgambin1@gmail.com>
2024-11-18 14:14:20 -0500

    add>

--- src/main/java/com/unicar/Class_shedule/commons/Course/factory/FactoryCourse.java
+++ src/main/java/com/unicar/Class_shedule/commons/Course/factory/FactoryCourse.java
@@ -7,12 +7,26 @@ import org.springframework.stereotype.Component;
 @Component
 public class FactoryCourse {
     public CourseDto courseDto(CourseEntity courseEntity) {
+        // Asegurarse de que 'docenteCedula' obtiene el 'dni' del docente (si no es null)
+        String docenteCedula = (courseEntity.getDocent() != null) ? courseEntity.getDocent().getUserEntity().getDni() : null;
 
+        String fullName = (courseEntity.getDocent() != null) ? courseEntity.getDocent().getUserEntity().getFullName() : null;
+
+        String correo = (courseEntity.getDocent() != null) ? courseEntity.getDocent().getUserEntity().getEmail() : null;
+
+        // Asegúrate de que 'codigoHorario' obtiene el 'id' del horario (si no es null)
+        String codigoHorario = (courseEntity.getSchedule() != null) ? courseEntity.getSchedule().getId().toString() : null;
+
+        // Devuelves el DTO con los valores correspondientes
         return CourseDto.builder()
                 .id(courseEntity.getId())
                 .name(courseEntity.getName())
                 .level(courseEntity.getLevel())
                 .cantHrs(courseEntity.getCantHrs())
+                .docenteCedula(docenteCedula)
+                .fullName(fullName)
+                .correo(correo)// Campo para la cédula del docente
+                .codigoHorario(codigoHorario) // Campo para el código del horario
                 .build();
     }
 }
--- src/main/java/com/unicar/Class_shedule/commons/Course/presentation/dto/CourseDto.java
+++ src/main/java/com/unicar/Class_shedule/commons/Course/presentation/dto/CourseDto.java
@@ -2,12 +2,19 @@ package com.unicar.Class_shedule.commons.Course.presentation.dto;
 
 import lombok.Builder;
 
+import lombok.Builder;
+
 @Builder
 public record CourseDto(
         Long id,
         String name,
         Integer cantHrs,
-        String level
+        String level,
+        String docenteCedula,  // Campo para la cédula del docente
+        String codigoHorario ,
+        String fullName,
+        String correo
 
+        // Campo para el código del horario
 ) {
 }
